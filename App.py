243d5c9af6f6381411b11a37e8d81648b25e7685
stepanPise <stepan.pise@gmail.com>
2026-01-30 00:21:05 +0100

    style: rearange UI elements to be more readable (sidebar, gallery)

--- App.py
+++ App.py
@@ -37,38 +37,47 @@ class PhotoApp(ctk.CTk):
 
     def create_widgets(self):
 
-        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=0, minsize=350)  # sidebar
+        self.grid_columnconfigure(1, weight=1)  # gallery
+
         self.grid_rowconfigure(0, weight=0)  # top_frame
         self.grid_rowconfigure(1, weight=0)  # lbl_folder
         self.grid_rowconfigure(2, weight=0)  # status_frame
-        self.grid_rowconfigure(3, weight=1)  # scroll_frame_people_people
-        self.grid_rowconfigure(4, weight=1)  # scroll_frame_photos
+        self.grid_rowconfigure(3, weight=1)  # SCROLL FRAMES
         self.grid_rowconfigure(5, weight=0)  # btn_export
 
         # Top frame
         top_frame = ctk.CTkFrame(self)
-        top_frame.grid(row=0, column=0, padx=20, pady=20, sticky="ew")
+        top_frame.grid(row=0, column=0, columnspan=2,
+                       padx=20, pady=20, sticky="ew")
         top_frame.grid_columnconfigure(2, weight=1)
 
         # Top frame item 1 = Select folder button
         self.btn_select_folder = ctk.CTkButton(
             top_frame, text="Select Folder", command=self.choose_folder)
-        self.btn_select_folder.grid(row=0, column=0, padx=10, sticky="w")
+        self.btn_select_folder.grid(
+            row=0, column=0, padx=10, pady=5, sticky="w")
 
         # Top frame item 2 = Detect faces switch
         self.switch_detect = ctk.CTkSwitch(
             top_frame, text="Detect Faces", variable=self.detect_faces_enabled)
-        self.switch_detect.grid(row=0, column=1, padx=10, sticky="w")
+        self.switch_detect.grid(row=0, column=1, padx=10, pady=5, sticky="w")
+
+        # Export button (not implemented)
+        self.btn_export = ctk.CTkButton(top_frame, text="Export Photos (TODO)")
+        self.btn_export.grid(row=0, column=3, padx=10, pady=5, sticky="e")
 
         # Selected folder string
         self.lbl_folder = ctk.CTkLabel(
             self, textvariable=self.selected_folder, text_color="gray"
         )
-        self.lbl_folder.grid(row=1, column=0, padx=20, sticky="w")
+        self.lbl_folder.grid(row=1, column=0, columnspan=2,
+                             padx=20, sticky="w")
 
         # Status frame for progress bar and status label
         self.status_frame = ctk.CTkFrame(self, fg_color="transparent")
-        self.status_frame.grid(row=2, column=0, sticky="ew", padx=20, pady=5)
+        self.status_frame.grid(
+            row=2, column=0, columnspan=2, sticky="ew", padx=20, pady=5)
         self.status_frame.grid_columnconfigure(0, weight=1)
 
         # Progress bar
@@ -87,7 +96,7 @@ class PhotoApp(ctk.CTk):
             self, self.controller
         )
         self.sidebar.grid(
-            row=3, column=0, padx=10, pady=10, sticky="nsew"
+            row=3, column=0, padx=(10, 5), pady=10, sticky="nsew"
         )
         self.sidebar.grid_columnconfigure(0, weight=1)
 
@@ -97,13 +106,9 @@ class PhotoApp(ctk.CTk):
             controller=self.controller,
         )
         self.gallery.grid(
-            row=4, column=0, padx=10, pady=10, sticky="nsew"
+            row=3, column=1, padx=(5, 10), pady=10, sticky="nsew"
         )
         self.gallery.grid_columnconfigure(0, weight=1)
-
-        # Export button (not implemented)
-        self.btn_export = ctk.CTkButton(self, text="Export Photos (TODO)")
-        self.btn_export.grid(row=5, column=0, pady=10)
 
     def choose_folder(self):
         folder = filedialog.askdirectory()
